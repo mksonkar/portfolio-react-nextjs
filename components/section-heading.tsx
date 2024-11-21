97c35b0b63fba29652cb2611e37957c8483765ef
Mukesh Kumar <mkso13189@gmail.com>
2024-11-21 14:13:09 +0530

    Define type for SectionHeading

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function SectionHeading({ children }) {
+type SectionHeadingProps = {
+  children: React.ReactNode;
+};
+
+export default function SectionHeading({ children }: SectionHeadingProps) {
   return <h2 className="mb-8 text-3xl font-medium capitalize">{children}</h2>;
 }
