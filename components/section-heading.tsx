481ff2a7d441bd554f27c880d33c9a2308711ed5
Mukesh Kumar <mkso13189@gmail.com>
2024-11-21 16:07:54 +0530

    Make section heading center

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -5,5 +5,9 @@ type SectionHeadingProps = {
 };
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
-  return <h2 className="mb-8 text-3xl font-medium capitalize">{children}</h2>;
+  return (
+    <h2 className="mb-8 text-center text-3xl font-medium capitalize">
+      {children}
+    </h2>
+  );
 }
