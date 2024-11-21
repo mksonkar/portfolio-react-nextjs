f7763142be97edc761a9c60105705976008e604e
Mukesh Kumar <mkso13189@gmail.com>
2024-11-21 12:51:12 +0530

    Change p to h1 for hero section

--- components/intro.jsx
+++ components/intro.jsx
@@ -45,7 +45,7 @@ function Intro() {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -54,7 +54,7 @@ function Intro() {
         <span className="font-bold">front-end developer.</span> I enjoy building
         cool apps and websites using{" "}
         <span className="underline">React and Next.js</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row"
