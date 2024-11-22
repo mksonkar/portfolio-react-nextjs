b277cb45316b97dd92a424b5f2a0e6affb00d263
Mukesh Kumar <mkso13189@gmail.com>
2024-11-22 15:52:18 +0530

    Add timeOfLastClick state to context

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -8,6 +8,8 @@ type ActiveSectionContextProviderProps = { children: React.ReactNode };
 type ActiveSectionContextType = {
   activeSection: SectionName;
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export const ActiveSectionContext =
@@ -17,8 +19,17 @@ export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+
   return (
-    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
       {children}
     </ActiveSectionContext.Provider>
   );
