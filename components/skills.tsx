c16d8fcbd78fead7957584d9275b159f2c5a0c49
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 17:13:07 +0530

    Use custom borderBlack class

--- components/skills.tsx
+++ components/skills.tsx
@@ -34,7 +34,7 @@ export default function Skills() {
         {skillsData.map((skill, index) => (
           <motion.li
             key={index}
-            className="rounded-xl border border-black/10 bg-white px-5 py-3"
+            className="rounded-xl borderBlack bg-white px-5 py-3"
             variants={fadeInAnimationVariants}
             initial="initial"
             whileInView="animate"
