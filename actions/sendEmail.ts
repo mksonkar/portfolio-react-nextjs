3c201f3d76350c4ec3d8fe55653f2c71ea7a6083
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 20:01:55 +0530

    Add function to validate string in form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,24 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
-  "use server";
-  console.log("Running on server");
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  if (!message || typeof message !== "string") {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "mkso13189@gmail.com",
+    subject: "From Portfolio",
+    replyTo: senderEmail,
+    text: message,
+  });
 };
