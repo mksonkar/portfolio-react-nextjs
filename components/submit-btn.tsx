d0e0af48d95b319a0f7d7b18a6c66d2ba4a75570
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 22:21:37 +0530

    Abstract submit btn to use useFormStatus hook

---
+++ components/submit-btn.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useFormStatus } from "react-dom";
+import { FaPaperPlane } from "react-icons/fa";
+
+export default function SubmitBtn() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      className="disabled:bg-opacity-65 group flex h-12 w-32 items-center justify-center gap-2 rounded-full bg-gray-900 text-white outline-none transition-all hover:scale-110 hover:bg-gray-950 focus:scale-110 disabled:scale-100"
+      disabled={pending}
+    >
+      {pending ? (
+        <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
+      ) : (
+        <>
+          Send{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:-translate-y-1 group-hover:translate-x-1" />
+        </>
+      )}
+    </button>
+  );
+}
