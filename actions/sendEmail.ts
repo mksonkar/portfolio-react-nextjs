5557b337b55770201488fc5d21b0db6de55dfa8a
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 20:02:50 +0530

    Add server side form validation and error handling

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { Resend } from "resend";
+import { getErrorMessage, validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -8,17 +9,28 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  if (!message || typeof message !== "string") {
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid email",
+    };
+  }
+  if (!validateString(message, 5000)) {
     return {
       error: "Invalid message",
     };
   }
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "mkso13189@gmail.com",
-    subject: "From Portfolio",
-    replyTo: senderEmail,
-    text: message,
-  });
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "mkso13189@gmail.com",
+      subject: "From Portfolio",
+      replyTo: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 };
