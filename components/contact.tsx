67388870dd21142e3e96f04340b2063339b8bf76
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 16:15:49 +0530

    Create contact component

---
+++ components/contact.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+
+export default function Contact() {
+  return (
+    <section>
+      <SectionHeading>Contact</SectionHeading>
+    </section>
+  );
+}
