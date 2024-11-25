38126f6bca31f2cd8ee31a5f5c83773dd5d27392
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 20:22:46 +0530

    Render react email component

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,9 @@
 "use server";
 
+import React from "react";
 import { Resend } from "resend";
 import { getErrorMessage, validateString } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -26,7 +28,10 @@ export const sendEmail = async (formData: FormData) => {
       to: "mkso13189@gmail.com",
       subject: "From Portfolio",
       replyTo: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error: unknown) {
     return {
