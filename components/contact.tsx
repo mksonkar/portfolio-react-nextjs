1177418be664bc7ac5cfc5ddd67ccdbea63c8ef8
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 18:54:24 +0530

    Add form validation

--- components/contact.tsx
+++ components/contact.tsx
@@ -26,15 +26,27 @@ export default function Contact() {
         </a>{" "}
         or through this form
       </p>
-      <form className="mt-10 flex flex-col">
+      <form
+        action={(formData) => {
+          console.log(formData.get("senderEmail"));
+          console.log(formData.get("message"));
+        }}
+        className="mt-10 flex flex-col"
+      >
         <input
+          name="senderEmail"
           type="email"
           placeholder="Enter your email"
           className="borderBlack h-14 rounded-lg px-4"
+          required
+          maxLength={500}
         />
         <textarea
+          name="message"
           className="borderBlack my-3 h-52 rounded-lg p-4"
           placeholder="Enter your message"
+          required
+          maxLength={500}
         />
         <button
           type="submit"
