e7922e95e7d767929306998f1cbdb17be0d52c6a
Mukesh Kumar <mkso13189@gmail.com>
2024-11-25 23:27:46 +0530

    Design the footer component

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">&copy; 2024 Mukesh Kumar.</small>
+      <p className="text-xs">
+        Built using React, Next.js (App router and Server Actions), TypeScript,
+        Tailwind CSS, Framer Motion, React Email & Resend, hosted on Vercel
+      </p>
+    </footer>
+  );
+}
