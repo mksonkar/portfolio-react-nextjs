6ad15a9441c132b1a96a96126079f810e63a276e
Mukesh Kumar <mkso13189@gmail.com>
2024-11-26 12:12:17 +0530

    Create theme context and move logic there

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,35 +1,15 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useThemeContext } from "@/context/theme-context";
+import React from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
-type theme = "light" | "dark";
-
 export default function ThemeSwitch() {
-  const [theme, setTheme] = useState<theme>("light");
-
-  useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme") as theme | null;
-    if (localTheme) {
-      setTheme(localTheme);
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-      window.localStorage.setItem("theme", "dark");
-    } else {
-      setTheme("light");
-      window.localStorage.setItem("theme", "light");
-    }
-  };
+  const { theme, toggleTheme } = useThemeContext();
 
   return (
     <button
-      className="hover:scale-115 fixed bottom-5 right-5 flex h-12 w-12 items-center justify-center rounded-full border border-white border-opacity-80 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem] transition-all active:scale-105"
+      className="hover:scale-115 fixed bottom-5 right-5 flex h-12 w-12 items-center justify-center rounded-full border border-gray-100 border-opacity-80 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem] transition-all active:scale-105 dark:border-white/20 dark:bg-gray-950 dark:text-yellow-100"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
