53318cbced1fbe9a4aaec8dae1b04b09fd098277
Mukesh Kumar <mkso13189@gmail.com>
2024-11-27 23:50:23 +0530

    Add new colors to the theme toggle button

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -2,17 +2,17 @@
 
 import { useThemeContext } from "@/context/theme-context";
 import React from "react";
-import { BsMoon, BsSun } from "react-icons/bs";
+import { BsMoonStars, BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useThemeContext();
 
   return (
     <button
-      className="hover:scale-115 fixed bottom-5 right-5 flex h-12 w-12 items-center justify-center rounded-full border border-gray-100 border-opacity-80 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem] transition-all active:scale-105 dark:border-white/20 dark:bg-gray-950 dark:text-yellow-100"
+      className="hover:scale-115 fixed bottom-5 right-5 flex h-12 w-12 items-center justify-center rounded-full border border-[#99582A] border-opacity-10 bg-[#BB9457] bg-opacity-40 shadow-2xl backdrop-blur-[0.5rem] transition-all active:scale-105  dark:bg-[#FFE6A7]/20 dark:text-[#ffe6a7]/70"
       onClick={toggleTheme}
     >
-      {theme === "light" ? <BsSun /> : <BsMoon />}
+      {theme === "light" ? <BsSun size={22} /> : <BsMoonStars size={22} />}
     </button>
   );
 }
